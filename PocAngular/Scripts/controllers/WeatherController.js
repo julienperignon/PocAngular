1d28e31225c4f83e98330568dd73a9c25cb28866
Julien PERIGNON <julien.perignon@netapsys.fr>
2015-04-23 21:07:33 +0200

    first commit ! it works !

---
+++ PocAngular/Scripts/controllers/WeatherController.js
@@ -0,0 +1,25 @@
+﻿var weatherApp = angular.module('weatherApp', []);
+
+weatherApp.factory('weatherService', function ($http) {
+    return {
+        getWeather: function () {
+            //return the promise directly.
+            return $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk')
+                      .then(function (result) {
+                          //resolve the promise as the data
+                          return result.data;
+                      });
+        }
+    }
+});
+
+weatherApp.controller('WeatherController', function ($scope, weatherService) {
+    console.log("hello");
+    $scope.test = "hello world";
+
+    weatherService.getWeather().then(function (weather) {
+        $scope.weather = weather;
+    });
+
+   console.log("after");
+});
